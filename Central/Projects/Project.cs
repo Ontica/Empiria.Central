8a4b4a70dc2bbab6714b69ea63fec45a28363c13
José Manuel Cota <jmcota@ontica.org>
2024-10-12 12:37:29 -0600

    Rfx Project type was moved from Empiria.FinancialManagement

---
+++ Central/Projects/Project.cs
@@ -0,0 +1,114 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Projects                                   Component : Domain Layer                            *
+*  Assembly : Empiria.Central.Core.dll                   Pattern   : Information Holder                      *
+*  Type     : Project                                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Holds basic information about a project.                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.StateEnums;
+
+namespace Empiria.Projects {
+
+  /// <summary>Holds basic information about a project.</summary>
+  public class Project : GeneralObject {
+
+    #region Constructors and parsers
+
+    internal Project(ProjectType projectType, string name) {
+      Assertion.Require(projectType, nameof(projectType));
+      Assertion.Require(name, nameof(name));
+
+      this.ProjectType = projectType;
+      this.Name = name;
+    }
+
+    static public Project Parse(int id) {
+      return BaseObject.ParseId<Project>(id);
+    }
+
+    static public Project Parse(string typeUID) => ParseKey<Project>(typeUID);
+
+    static public FixedList<Project> GetList() {
+      return BaseObject.GetList<Project>(string.Empty, string.Empty)
+                       .FindAll(x => x.Status == StateEnums.EntityStatus.Active)
+                       .ToFixedList();
+    }
+
+    static public FixedList<Project> GetListFor(ProjectType projectType) {
+      return BaseObject.GetList<Project>()
+                       .ToFixedList();
+    }
+
+    static public Project Empty => ParseEmpty<Project>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public ProjectType ProjectType {
+      get {
+        return ExtendedDataField.Get<ProjectType>("projectTypeId");
+      }
+      private set {
+        ExtendedDataField.Set("projectTypeId", value.Id);
+      }
+    }
+
+
+    public string Code {
+      get {
+        return ExtendedDataField.Get("code", "N/D");
+      }
+      private set {
+        ExtendedDataField.SetIfValue("code", value);
+      }
+    }
+
+
+    public string Description {
+      get {
+        return ExtendedDataField.Get("description", "N/D");
+      }
+      private set {
+        ExtendedDataField.SetIfValue("description", value);
+      }
+    }
+
+    public override string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(base.Keywords, Code, ProjectType.Name, Description);
+      }
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    internal void Delete() {
+      this.Status = EntityStatus.Deleted;
+
+      MarkAsDirty();
+    }
+
+
+    internal void Update(ProjectFields fields) {
+      Assertion.Require(fields, nameof(fields));
+
+      fields.EnsureValid();
+
+      ProjectType = PatchField(fields.ProjectTypeUID, this.ProjectType);
+      Code = PatchCleanField(fields.Code, this.Code);
+      Name = PatchCleanField(fields.Name, this.Name);
+      Description = PatchCleanField(fields.Description, this.Description);
+
+      MarkAsDirty();
+    }
+
+    #endregion Methods
+
+  }  // class Project
+
+}  // namespace Empiria.Projects
---
+++ Central/Projects/ProjectFields.cs
@@ -0,0 +1,47 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Projects                                   Component : Adapters Layer                          *
+*  Assembly : Empiria.Central.dll                        Pattern   : Input Fields DTO                        *
+*  Type     : ProjectFields                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Input fields DTO used for update project's information.                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Projects {
+
+  /// <summary>Input fields DTO used for update project's information.</summary>
+  public class ProjectFields : NamedEntityFields {
+
+    public string ProjectTypeUID {
+      get; set;
+    }
+
+    public string Code {
+      get; set;
+    }
+
+    public string Description {
+      get; set;
+    }
+
+  }  // class ProjectFields
+
+
+
+  /// <summary>Extension methods for ProjectFields type.</summary>
+  static internal class ProjectFieldsExtensions {
+
+    static internal void EnsureValid(this ProjectFields fields) {
+      if (fields.ProjectTypeUID.Length != 0) {
+        _ = fields.GetProjectType();
+      }
+    }
+
+    static internal ProjectType GetProjectType(this ProjectFields fields) {
+      return ProjectType.Parse(fields.ProjectTypeUID);
+    }
+
+  }  // class ProjectFieldsExtensions
+
+}  // namespace Empiria.Projects
