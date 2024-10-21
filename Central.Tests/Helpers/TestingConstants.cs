6e7516bf4edb49a3d25ba2236fba7da189d7a586
José Manuel Cota <jmcota@ontica.org>
2024-10-20 23:04:32 -0600

    TestingConstants added and its namespace changed

--- Central.Tests/Helpers/TestingConstants.cs
+++ Central.Tests/Helpers/TestingConstants.cs
@@ -8,11 +8,14 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
-namespace Empiria.Tests.Central {
+namespace Empiria.Tests {
 
   /// <summary>Provides testing constants for Empiria Central Framework types and components.</summary>
   static public class TestingConstants {
 
+    static internal readonly string PRODUCT_KIND_UID = "e5a8e60e-2a07-4817-b2f4-9b42083d5986";
+
+    static internal readonly string PRODUCT_UID = "39bf118b-aaf9-46f3-ae69-e871ac9d9c50";
 
   }  // class TestingConstants
 
