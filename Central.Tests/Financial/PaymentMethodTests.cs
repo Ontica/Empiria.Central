0a5fbd9bd3fb61f7920f9ef6198c08d123976ffa
cyanez <cyanez@ontica.org>
2024-10-22 21:38:15 -0600

    Add PaymentMethod class

---
+++ Central.Tests/Financial/PaymentMethodTests.cs
@@ -0,0 +1,42 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial                                  Component : Test cases                              *
+*  Assembly : Empiria.Central.Tests.dll                  Pattern   : Unit tests                              *
+*  Type     : CataloguesUseCasesTests                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for Currency type.                                                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Financial;
+
+namespace Empiria.Tests.Financial {
+
+  /// <summary>Unit tests for Currency type.</summary>
+  public class PaymentMethodTests {
+
+    #region Facts
+
+    [Fact]
+    public void Should_Parse_Empty_PaymentMethod() {
+      var sut = PaymentMethod.Empty;
+
+      Assert.NotNull(sut);
+    }
+
+
+    [Fact]
+    public void Should_Get_PaymentMethods() {
+      var sut = PaymentMethod.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+    #endregion Facts
+
+  }  // class CurrencyTests
+
+}  // namespace Empiria.Tests.Financial
---
+++ Central.WebApi/Financial/PaymentMethodController.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Financial                                    Component : Web Api                               *
+*  Assembly : Empiria.Central.WebApi.dll                   Pattern   : Query web api controller              *
+*  Type     : CurrencyController                           License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Query web API used to retrive currencies.                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+namespace Empiria.Financial.WebApi {
+
+  /// <summary>Query web API used to retrive currencies.</summary>
+  public class PaymentMethodController : WebApiController {
+
+    #region Query web apis
+ 
+    [HttpGet]
+    [Route("v2/payments-management/payment-methods")]
+
+    [Route("v2/financial/catalogues/payment-methods")]
+    [Route("v2/financial/payment-methods")]
+    public CollectionModel GetPaymentMethods() {
+  
+      FixedList<PaymentMethod> paymentMethods = PaymentMethod.GetList();
+
+      return new CollectionModel(Request, paymentMethods.MapToNamedEntityList());
+    }
+
+    #endregion Query web apis
+
+  }  // class CurrencyController
+
+}  // namespace Empiria.Financial.WebApi
