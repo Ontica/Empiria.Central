2c6963afde385772cfde925a4a65dc282cc191c7
José Manuel Cota <jmcota@ontica.org>
2024-10-31 21:15:37 -0600

    HistoryServices initial version added

---
+++ Central.Services/History/Adapters/HistoryDto.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Documents                                  Component : Adapters Layer                          *
+*  Assembly : Empiria.Central.Services.dll               Pattern   : Output DTO                              *
+*  Type     : DocumentDto                                License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Output DTO for Document instances.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+namespace Empiria.History.Services.Adapters {
+
+  // <summary>Output DTO used to return minimal history information about payables.</summary>
+  public class HistoryDto {
+
+    public string UID {
+      get; internal set;
+    }
+
+
+    public string Type {
+      get; internal set;
+    }
+
+
+    public string Description {
+      get; internal set;
+    }
+
+
+    public DateTime Time {
+      get; internal set;
+    }
+
+
+    public NamedEntityDto Party {
+      get; internal set;
+    }
+
+  } // HistoryDto
+
+}  // namespace Empiria.DocumentDto.Services.Adapters
---
+++ Central.Services/History/Services/HistoryServices.cs
@@ -0,0 +1,42 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Documents                                  Component : Services Layer                          *
+*  Assembly : Empiria.Central.Services.dll               Pattern   : Services interactor class               *
+*  Type     : DocumentServices                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Services for Document instances.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+using Empiria.History.Services.Adapters;
+using Empiria.Services;
+
+namespace Empiria.History.Services {
+
+  /// <summary>Services for Document instances.</summary>
+  public class HistoryServices : Service {
+
+    #region Constructors and parsers
+
+    protected HistoryServices() {
+      // no-op
+    }
+
+    static public HistoryServices ServiceInteractor() {
+      return Service.CreateInstance<HistoryServices>();
+    }
+
+    public FixedList<HistoryDto> GetEntityHistory(BaseObject entity) {
+      return new FixedList<HistoryDto>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Services
+
+    #endregion Services
+
+  }  // class HistoryServices
+
+}  // namespace Empiria.Documents.Services
