3365dfc126857e9f8401aeb5ed543ea98ba8d7f8
José Manuel Cota <jmcota@ontica.org>
2024-11-17 10:55:06 -0600

    Location class added to represent physical locations

---
+++ Central.Tests/Locations/LocationTests.cs
@@ -0,0 +1,54 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Locations                                  Component : Test cases                              *
+*  Assembly : Empiria.Central.Tests.dll                  Pattern   : Unit tests                              *
+*  Type     : LocationTests                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for Location type.                                                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Locations;
+
+namespace Empiria.Tests.Locations {
+
+  /// <summary>Unit tests for Location type.</summary>
+  public class LocationTests {
+
+    #region Facts
+
+    [Fact]
+    public void Should_Get_All_Locations() {
+      var sut = Location.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Parse_All_Locations() {
+      var list = Location.GetList();
+
+      foreach (var sut in list) {
+        Assert.NotNull(sut.LocationType);
+        Assert.NotNull(sut.Parent);
+      }
+
+    }
+
+
+    [Fact]
+    public void Should_Parse_Empty_Location() {
+      var sut = Location.Empty;
+
+      Assert.NotNull(sut);
+    }
+
+    #endregion Facts
+
+  }  // class LocationTests
+
+}  // namespace Empiria.Tests.Locations
---
+++ Central/Locations/Location.cs
@@ -0,0 +1,75 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Locations                                  Component : Domain Layer                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Information Holder                      *
+*  Type     : Location                                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a physical location like a building, an office or work place, a store               *
+*             or a warehouse, etc. Locations are fractal objects with a parent and zero ore more children.   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Locations {
+
+  /// <summary>Represents a physical location like a building, an office or work place, a store
+  /// or a warehouse, etc. Locations are fractal objects with a parent and zero ore more children.</summary>
+  public class Location : CommonStorage {
+
+    #region Constructors and parsers
+
+    static public Location Parse(int id) => ParseId<Location>(id);
+
+    static public Location Parse(string uid) => ParseKey<Location>(uid);
+
+    static public FixedList<Location> GetList() {
+      return BaseObject.GetList<Location>()
+                       .ToFixedList();
+    }
+
+    static public Location Empty => ParseEmpty<Location>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public string Code {
+      get {
+        return base.Data.Code;
+      }
+      private set {
+        base.Data.Code = EmpiriaString.Clean(value);
+      }
+    }
+
+
+    public override string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(base.Keywords, Code);
+      }
+    }
+
+
+    public LocationType LocationType {
+      get {
+        return LocationType.Parse(base.Data.ObjectClassificationId);
+      }
+      private set {
+        base.Data.ObjectClassificationId = value.Id;
+      }
+    }
+
+
+    public Location Parent {
+      get {
+        return Location.Parse(base.Data.ParentObjectId);
+      }
+      private set {
+        base.Data.ParentObjectId = value.Id;
+      }
+    }
+
+    #endregion Properties
+
+  } // class Location
+
+} // namespace Empiria.Locations
