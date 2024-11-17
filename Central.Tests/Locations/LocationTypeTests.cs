8e98c1372311cee4ca32a231da08cdbc6e71e720
José Manuel Cota <jmcota@ontica.org>
2024-11-17 10:54:00 -0600

    LocationType class added

---
+++ Central.Tests/Locations/LocationTypeTests.cs
@@ -0,0 +1,42 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Locations                                  Component : Test cases                              *
+*  Assembly : Empiria.Central.Tests.dll                  Pattern   : Unit tests                              *
+*  Type     : LocationTypeTests                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for LocationType.                                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Locations;
+
+namespace Empiria.Tests.Locations {
+
+  /// <summary>Unit tests for LocationType.</summary>
+  public class LocationTypeTests {
+
+    #region Facts
+
+    [Fact]
+    public void Should_Get_All_Location_Types() {
+      var sut = LocationType.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Parse_Empty_LocationType() {
+      var sut = LocationType.Empty;
+
+      Assert.NotNull(sut);
+    }
+
+    #endregion Facts
+
+  }  // class LocationTypeTests
+
+}  // namespace Empiria.Tests.Locations
---
+++ Central/Locations/LocationType.cs
@@ -0,0 +1,37 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Locations                                  Component : Domain Layer                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Information Holder                      *
+*  Type     : LocationType                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes a location type like warehouse, building, office, workarea, store, etc.              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Locations {
+
+  /// <summary>Describes a location type like warehouse, building, office, workarea, store, etc.</summary>
+  public class LocationType : CommonStorage {
+
+    #region Constructors and parsers
+
+    protected LocationType() {
+      // Required by Empiria Framework
+    }
+
+    static public LocationType Parse(int id) => ParseId<LocationType>(id);
+
+    static public LocationType Parse(string uid) => ParseKey<LocationType>(uid);
+
+    static public FixedList<LocationType> GetList() {
+      return BaseObject.GetList<LocationType>()
+                       .ToFixedList();
+    }
+
+    static public LocationType Empty => ParseEmpty<LocationType>();
+
+    #endregion Constructors and parsers
+
+  } // class LocationType
+
+} // namespace Empiria.Locations
