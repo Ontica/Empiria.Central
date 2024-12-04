4c29e6e0cd5e1cd3b356e7af14221a6737f4ca9a
José Manuel Cota <jmcota@ontica.org>
2024-12-04 13:50:04 -0600

    IPayable interface added

---
+++ Central/Financial/IPayable.cs
@@ -0,0 +1,26 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Financial                                  Component : Domain Layer                            *
+*  Assembly : Empiria.Central.Core.dll                   Pattern   : Integration interface                   *
+*  Type     : IPayable                                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Defines a payable.                                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial {
+
+  /// <summary>Defines a payable.</summary>
+  public interface IPayable {
+
+    int Id {
+      get;
+    }
+
+    IPayableEntity PayableEntity {
+      get;
+    }
+
+  }  // interface IPayable
+
+}  // namespace Empiria.Financial
