0c0f5df185fc63b20ad975b212ddbc4f891c0b4d
José Manuel Cota <jmcota@ontica.org>
2024-12-10 08:00:36 -0600

    ProductUnit web api controller to get product units was added

---
+++ Central.WebApi/Products/ProductUnitController.cs
@@ -0,0 +1,35 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Products                                     Component : Web Api                               *
+*  Assembly : Empiria.Central.WebApi.dll                   Pattern   : Web api controller                    *
+*  Type     : ProductUnitController                        License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Query web API used to retrive and update product units.                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+namespace Empiria.Products.WebApi {
+
+  /// <summary>Query web API used to retrive and update product units.</summary>
+  public class ProductUnitController : WebApiController {
+
+    #region Query web apis
+
+    [HttpGet]
+    [Route("v8/products/product-units")]
+    public CollectionModel SearchProductUnits([FromUri] string keywords = "") {
+
+      FixedList<ProductUnit> productUnits = BaseObject.GetFullList<ProductUnit>();
+
+      return new CollectionModel(Request, productUnits.MapToNamedEntityList());
+    }
+
+    #endregion Query web apis
+
+  }  // class ProductUnitController
+
+}  // namespace Empiria.Products.WebApi
