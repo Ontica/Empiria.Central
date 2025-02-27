1faa8fb3f51423fe37e3311d17e9e53a4e9ddd6c
José Manuel Cota <jmcota@ontica.org>
2025-02-27 10:46:25 -0500

    HumanResources.Employment type added

---
+++ Central.Tests/HumanResources/EmploymentTests.cs
@@ -0,0 +1,56 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Human Resources                            Component : Test cases                              *
+*  Assembly : Empiria.Central.Tests.dll                  Pattern   : Unit tests                              *
+*  Type     : EmploymentTests                            License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for Employment instances.                                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Parties;
+
+using Empiria.HumanResources;
+
+namespace Empiria.Tests.HumanResources {
+
+  /// <summary>Unit tests for Employment instances.</summary>
+  public class EmploymentTests {
+
+    [Fact]
+    public void Should_Get_Empty_Employment() {
+      var sut = Employment.Empty;
+
+      Assert.Equal(-1, sut.Id);
+      Assert.Equal("Empty", sut.UID);
+      Assert.Equal("N/D", sut.EmployeeNo);
+    }
+
+
+    [Fact]
+    public void Should_Get_All_Employments() {
+      var sut = BaseObject.GetList<Employment>();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Parse_All_Employments() {
+      var employments = BaseObject.GetList<Employment>();
+
+      foreach (var sut in employments) {
+        Assert.NotEqual(Party.Empty, sut.Employer);
+        Assert.NotEqual(Party.Empty, sut.Employee);
+        Assert.True(typeof(Organization).Equals(sut.Employer.GetType()) ||
+                    typeof(Person).Equals(sut.Employer.GetType()));
+        Assert.NotEmpty(sut.EmployeeNo);
+      }
+    }
+
+  }  // class EmploymentTests
+
+}  // namespace Empiria.Tests.HumanResources
---
+++ Central/HumanResources/Employment.cs
@@ -0,0 +1,79 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Human Resources                            Component : Domain Layer                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Information Holder                      *
+*  Type     : Employment                                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an employment as a relationship between an organization (or person) and a person.   *
+*             An employment relation has one or many position assignments.                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Parties;
+
+namespace Empiria.HumanResources {
+
+  /// <summary>Represents an employment as a relationship between an organization (or person) and a person.
+  /// An employment relation has one or many position assignments.</summary>
+  public class Employment : PartyRelation {
+
+    #region Constructors and parsers
+
+    protected Employment(PartyRelationType powertype) : base(powertype) {
+      // Required by Empiria Framework for all partitioned types.
+    }
+
+    static public new Employment Parse(int id) => ParseId<Employment>(id);
+
+    static public new Employment Parse(string uid) => ParseKey<Employment>(uid);
+
+    static public new Employment Empty => ParseEmpty<Employment>();
+
+    static public FixedList<Person> GetEmployees() {
+      var employments = GetList();
+
+      return employments.Select(e => e.Employee).ToFixedList();
+    }
+
+    static public FixedList<Employment> GetList() {
+      var employments = GetList<Employment>("PARTY_RELATION_STATUS <> 'X'");
+
+      return employments.ToFixedList();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public Party Employer {
+      get {
+        return Commissioner;
+      }
+    }
+
+
+    public Person Employee {
+      get {
+        return (Person) Responsible;
+      }
+    }
+
+
+    public string EmployeeNo {
+      get {
+        return base.ExtendedData.Get("employeeNo", "N/D");
+      }
+    }
+
+
+    public override string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(base.Keywords, EmployeeNo, Employee.Keywords, Employer.Keywords);
+      }
+    }
+
+    #endregion Properties
+
+  } // class Employment
+
+} // namespace Empiria.HumanResources
