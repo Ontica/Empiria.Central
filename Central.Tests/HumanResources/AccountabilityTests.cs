75de131ced4af71487c0b0624ddac52e068b13b5
José Manuel Cota <jmcota@ontica.org>
2025-06-12 22:41:29 -0600

    Accountability type added as a PartyRelation

---
+++ Central.Tests/HumanResources/AccountabilityTests.cs
@@ -0,0 +1,54 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Human Resources                            Component : Test cases                              *
+*  Assembly : Empiria.Central.Tests.dll                  Pattern   : Unit tests                              *
+*  Type     : AccountabilityTests                        License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for Accountability instances.                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.HumanResources;
+
+namespace Empiria.Tests.HumanResources {
+
+  /// <summary>Unit tests for Accountability instances.</summary>
+  public class AccountabilityTests {
+
+    [Fact]
+    public void Should_Get_Empty_Accountability() {
+      var sut = Accountability.Empty;
+
+      Assert.Equal(-1, sut.Id);
+      Assert.Equal("Empty", sut.UID);
+      Assert.Equal(Accountability.Parse("Empty"), sut);
+    }
+
+
+    [Fact]
+    public void Should_Get_All_Accountabilities() {
+      var sut = Accountability.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Parse_All_Accountabilities() {
+      var accountabilities = Accountability.GetList();
+
+      foreach (var sut in accountabilities) {
+        Assert.NotNull(sut.Commissioner);
+        Assert.NotNull(sut.Responsible);
+        Assert.NotEqual(sut.Commissioner, sut.Responsible);
+        Assert.NotNull(sut.Role);
+        Assert.True(sut.StartDate <= sut.EndDate);
+      }
+    }
+
+  }  // class AccountabilityTests
+
+}  // namespace Empiria.Tests.HumanResources
---
+++ Central/HumanResources/Domain/Accountability.cs
@@ -0,0 +1,74 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Human Resources                            Component : Domain Layer                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Information Holder                      *
+*  Type     : Accountability                             License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an accountability or reponsibility between a party and a person.                    *
+*                                                                                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Parties;
+
+namespace Empiria.HumanResources {
+
+  /// <summary>Represents an accountability or reponsibility between a party and a person.</summary>
+  public class Accountability : PartyRelation {
+
+    #region Constructors and parsers
+
+    protected Accountability(PartyRelationType powertype) : base(powertype) {
+      // Required by Empiria Framework for all partitioned types.
+    }
+
+    static public new Accountability Parse(int id) => ParseId<Accountability>(id);
+
+    static public new Accountability Parse(string uid) => ParseKey<Accountability>(uid);
+
+    static public new Accountability Empty => ParseEmpty<Accountability>();
+
+    static public FixedList<Accountability> GetList() {
+      var accountabilities = GetList<Accountability>("PARTY_RELATION_STATUS <> 'X'");
+
+      return accountabilities.ToFixedList();
+    }
+
+    static internal FixedList<Accountability> GetListFor(Party commissioner) {
+      Assertion.Require(commissioner, nameof(commissioner));
+      Assertion.Require(!commissioner.IsEmptyInstance, nameof(commissioner));
+
+      var accountabilities = GetList<Accountability>($"COMMISSIONER_PARTY_ID = {commissioner.Id} AND " +
+                                                     $"PARTY_RELATION_STATUS <> 'X'");
+
+      return accountabilities.ToFixedList();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public new Party Commissioner {
+      get {
+        return base.Commissioner;
+      }
+    }
+
+
+    public new Person Responsible {
+      get {
+        return (Person) base.Responsible;
+      }
+    }
+
+    public new PartyRole Role {
+      get {
+        return base.Role;
+      }
+    }
+
+    #endregion Properties
+
+  } // class Accountability
+
+} // namespace Empiria.HumanResources
