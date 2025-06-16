4d45dba1500330c597ae9129b3038bf094269830
José Manuel Cota <jmcota@ontica.org>
2025-06-16 11:32:16 -0400

    AccountabilityServices and web api controller added

---
+++ Central.WebApi/HumanResources/AccountabilityController.cs
@@ -0,0 +1,67 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Human Resources                              Component : Web Api                               *
+*  Assembly : Empiria.Central.WebApi.dll                   Pattern   : Web Api Controller                    *
+*  Type     : AccountabilityController                     License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web API used to retrive and update accountabilites.                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.Parties;
+using Empiria.WebApi;
+
+using Empiria.HumanResources;
+
+namespace Empiria.Products.WebApi {
+
+  /// <summary>Web API used to retrive and update accountabilites.</summary>
+  public class AccountabilityController : WebApiController {
+
+    #region Command Web Apis
+
+    [HttpPost]
+    [Route("v8/human-resources/accountabilities")]
+    public SingleObjectModel CreateAccountability([FromBody] PartyRelationFields fields) {
+
+      using (var services = AccountabilityServices.ServiceInteractor()) {
+        OrganizationalStructureHolder structure = services.CreateAccountability(fields);
+
+        return new SingleObjectModel(base.Request, structure);
+      }
+    }
+
+
+    [HttpDelete]
+    [Route("v8/human-resources/accountabilities/{accountabilityUID:guid}")]
+    public SingleObjectModel DeleteAccountability([FromUri] string accountabilityUID) {
+
+      using (var services = AccountabilityServices.ServiceInteractor()) {
+        OrganizationalStructureHolder structure = services.DeleteAccountability(accountabilityUID);
+
+        return new SingleObjectModel(base.Request, structure);
+      }
+    }
+
+
+    [HttpPut, HttpPatch]
+    [Route("v8/human-resources/accountabilities/{accountabilityUID:guid}")]
+    public SingleObjectModel UpdateAccountability([FromUri] string accountabilityUID,
+                                                  [FromBody] PartyRelationFields fields) {
+
+      fields.UID = accountabilityUID;
+
+      using (var services = AccountabilityServices.ServiceInteractor()) {
+        OrganizationalStructureHolder structure = services.UpdateAccountability(fields);
+
+        return new SingleObjectModel(base.Request, structure);
+      }
+    }
+
+    #endregion Command Web Apis
+
+  }  // class AccountabilityController
+
+}  // namespace Empiria.Products.WebApi
---
+++ Central/HumanResources/Services/AccountabilityServices.cs
@@ -0,0 +1,79 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Human Resources                            Component : Services Layer                          *
+*  Assembly : Empiria.Central.dll                        Pattern   : Services provider                       *
+*  Type     : AccountabilityServices                     License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides services to update and retrieve accountabilities.                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+using Empiria.Parties;
+
+namespace Empiria.HumanResources {
+
+  /// <summary>Provides services to update and retrieve accountabilities.</summary>
+  public class AccountabilityServices : Service {
+
+    #region Constructors and parsers
+
+    protected AccountabilityServices() {
+      // no-op
+    }
+
+    static public AccountabilityServices ServiceInteractor() {
+      return Service.CreateInstance<AccountabilityServices>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Services
+
+    public OrganizationalStructureHolder CreateAccountability(PartyRelationFields fields) {
+      Assertion.Require(fields, nameof(fields));
+
+      var accountability = new Accountability(fields.GetRole(),
+                                              fields.GetCommissioner(),
+                                              (Person) fields.GetResponsible());
+
+      accountability.Update(fields);
+
+      accountability.Save();
+
+      return OrganizationalUnitMapper.Map((OrganizationalUnit) accountability.Commissioner);
+    }
+
+
+    public OrganizationalStructureHolder DeleteAccountability(string accountabilityUID) {
+      Assertion.Require(accountabilityUID, nameof(accountabilityUID));
+
+      var accountability = Accountability.Parse(accountabilityUID);
+
+      accountability.Delete();
+
+      accountability.Save();
+
+      return OrganizationalUnitMapper.Map((OrganizationalUnit) accountability.Commissioner);
+    }
+
+
+    public OrganizationalStructureHolder UpdateAccountability(PartyRelationFields fields) {
+      Assertion.Require(fields, nameof(fields));
+
+      var accountability = Accountability.Parse(fields.UID);
+
+      accountability.Update(fields);
+
+      accountability.Save();
+
+      return OrganizationalUnitMapper.Map((OrganizationalUnit) accountability.Commissioner);
+    }
+
+
+    #endregion Services
+
+  }  // class AccountabilityServices
+
+}  // namespace Empiria.HumanResources
