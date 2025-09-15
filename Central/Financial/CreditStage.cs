febf0863b0b869ae6dd6f2ff0863a0ff9cafb739
José Manuel Cota <jmcota@ontica.org>
2025-09-15 19:26:27 -0400

    Rfx CreditStage and CreditType were moved from Financial.Management repo

---
+++ Central/Financial/CreditStage.cs
@@ -0,0 +1,32 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Financial                                  Component : Domain Types                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Common Storage Type                     *
+*  Type     : CreditStage                                License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes the stage of a credit.                                                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial {
+
+  /// <summary>Describes the stage of a credit.</summary>
+  public class CreditStage : CommonStorage {
+
+    #region Constructors and parsers
+
+    static public CreditStage Parse(int id) => ParseId<CreditStage>(id);
+
+    static public CreditStage Parse(string uid) => ParseKey<CreditStage>(uid);
+
+    static public CreditStage Empty => ParseEmpty<CreditStage>();
+
+    static public FixedList<CreditStage> GetList() {
+      return GetStorageObjects<CreditStage>();
+    }
+
+    #endregion Constructors and parsers
+
+  } // class CreditStage
+
+} // namespace Empiria.Financial
---
+++ Central/Financial/CreditType.cs
@@ -0,0 +1,32 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Financial                                  Component : Domain Types                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Common Storage Type                     *
+*  Type     : CreditType                                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes the type of a credit.                                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial {
+
+  /// <summary>Describes the type of a credit.</summary>
+  public class CreditType : CommonStorage {
+
+    #region Constructors and parsers
+
+    static public CreditType Parse(int id) => ParseId<CreditType>(id);
+
+    static public CreditType Parse(string uid) => ParseKey<CreditType>(uid);
+
+    static public CreditType Empty => ParseEmpty<CreditType>();
+
+    static public FixedList<CreditType> GetList() {
+      return GetStorageObjects<CreditType>();
+    }
+
+    #endregion Constructors and parsers
+
+  } // class CreditType
+
+} // namespace Empiria.Financial
