ca5ed8c36c2e661f630f04c1457a5ecc6dd1a83f
José Manuel Cota <jmcota@ontica.org>
2025-11-20 16:45:55 -0500

    Country type added with web api access

---
+++ Central.WebApi/Locations/CountriesController.cs
@@ -0,0 +1,35 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Locations                                     Component : Web Api                              *
+*  Assembly : Empiria.Central.WebApi.dll                    Pattern   : Query Web api controller             *
+*  Type     : CountriesController                           License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Web API used to retrive countries.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+namespace Empiria.Locations.WebApi {
+
+  /// <summary>Web API used to retrive countries.</summary>
+  public class CountriesController : WebApiController {
+
+    #region Query web apis
+
+    [HttpGet]
+    [Route("v8/locations/countries")]
+    public CollectionModel GetCountries() {
+
+      FixedList<Country> countries = Country.GetList();
+
+      return new CollectionModel(Request, countries.MapToNamedEntityList());
+    }
+
+    #endregion Query web apis
+
+  }  // class CountriesController
+
+}  // namespace Empiria.Locations.WebApi
---
+++ Central/Locations/Country.cs
@@ -0,0 +1,45 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Locations                                  Component : Domain Layer                            *
+*  Assembly : Empiria.Central.dll                        Pattern   : Information Holder                      *
+*  Type     : Country                                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a country.                                                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Locations {
+
+  /// <summary>Represents a country.</summary>
+  public class Country : CommonStorage {
+
+    #region Constructors and parsers
+
+    static public Country Parse(int id) => ParseId<Country>(id);
+
+    static public Country Parse(string uid) => ParseKey<Country>(uid);
+
+    static public FixedList<Country> GetList() {
+      return GetStorageObjects<Country>();
+    }
+
+    static public Country Empty => ParseEmpty<Country>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public string CountryCode {
+      get {
+        return base.Code;
+      }
+      private set {
+        base.Code = EmpiriaString.Clean(value);
+      }
+    }
+
+    #endregion Properties
+
+  } // class Country
+
+} // namespace Empiria.Locations
