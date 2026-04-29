1da4b900e179cc1fc85053b8e06645b5126777e7
José Manuel Cota <jmcota@ontica.org>
2026-04-29 15:29:34 -0400

    Rfx GET report-types controller was moved from Banobras.PYC.WebApi

--- Central.Tests/Documents/ReportTypeTests.cs
+++ Central.Tests/Documents/ReportTypeTests.cs
@@ -4,29 +4,27 @@
 *  Assembly : Empiria.Central.Tests.dll                  Pattern   : Unit tests                              *
 *  Type     : ReportTypeTests                            License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary  : Unit tests for Report Types instances.                                                         *
+*  Summary  : Unit tests for ReportType type.                                                                *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
-using Empiria.Documents;
-
-using Empiria.Documents.UseCases;
-
-
 using Xunit;
+
+using Empiria.Documents;
+using Empiria.Documents.UseCases;
 
 namespace Empiria.Tests.Documents {
 
-  /// <summary>Unit tests for Report Types instances.</summary>
+  /// <summary>Unit tests for ReportType type.</summary>
   public class ReportTypeTests {
-
-
 
     [Fact]
     public void Should_Get_All_ReportTypes_UseCase() {
+
       TestsCommonMethods.Authenticate();
 
       using (var useCase = ReportTypesUseCases.UseCaseInteractor()) {
+
         var sut = useCase.GetReportTypes();
 
         Assert.NotNull(sut);
---
+++ Central.WebApi/Documents/ReportTypeController.cs
@@ -0,0 +1,40 @@
+﻿/* Empiria Central *******************************************************************************************
+*                                                                                                            *
+*  Module   : Documents                                    Component : Web Api                               *
+*  Assembly : Empiria.Central.WebApi.dll                   Pattern   : Web Api Controller                    *
+*  Type     : ReportTypeController                         License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web API used to retrieve Report Types information.                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.Documents.UseCases;
+
+namespace Empiria.Documents.WebApi {
+
+  /// <summary>Web API used to retrieve Report Types information.</summary>
+  public class ReportTypeController : WebApiController {
+
+    #region Web Apis
+
+    [HttpGet]
+    [Route("v2/documents/report-types/")]
+    public CollectionModel GetReportTypes() {
+
+      using (var useCase = ReportTypesUseCases.UseCaseInteractor()) {
+
+        FixedList<ReportTypeDto> reportTypes = useCase.GetReportTypes();
+
+        return new CollectionModel(base.Request, reportTypes);
+      }
+    }
+
+    #endregion Web Apis
+
+  }  // class ReportTypeController
+
+}  // namespace Empiria.Documents.WebApi
